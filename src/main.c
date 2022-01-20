5387a79c2901fd82824cf27316533d503c6c6a10
Frederik SCHLUTER <fschlute@e2r7p4.clusters.42paris.fr>
2022-01-20 12:11:29 +0100

    refactoring markup and introducing second index mode. Needs a little debugging, though

--- src/main.c
+++ src/main.c
@@ -1,15 +1,24 @@
 #include "libft/libft.h"
 #include "push_swap.h"
 
+// TODO: do i need to process the return value of the copy process?
+void init_russian_algorithm(t_meta_stack *a, t_meta_stack **b, t_list **history,
+							t_list **a_stack_cpy)
+{
+	(*a_stack_cpy) = ft_lstmap(a->stack, shallow_copy, do_not_free_content);
+	(*b) = new_meta_stack();
+	(*b)->is_stack_a = 0;
+	(*b)->stack = NULL;
+	(*history) = NULL;
+}
+
 t_list *russian_algorithm(t_meta_stack *a, enum markup_mode markup_mode)
 {
 	t_meta_stack	*b;
 	t_list	*history;
+	t_list			*a_stack_cpy;
 
-	b = new_meta_stack();
-	b->is_stack_a = 0;
-	b->stack = NULL;
-	history = NULL;
+	init_russian_algorithm(a, &b, &history, &a_stack_cpy);
 	ft_putendl_fd("------------>a input", 1);
 	ft_lstput_nbr_bonus(a->stack);
 	fill_b(a, b, &history, markup_mode);
@@ -26,6 +35,10 @@ t_list *russian_algorithm(t_meta_stack *a, enum markup_mode markup_mode)
 	rotate_a_back_in_order(a, &history);
 	ft_putendl_fd("------------>a sorted", 1);
 	ft_lstput_nbr_bonus(a->stack);
+	free(b);
+	ft_lstclear(&a->stack, do_not_free_content);
+	a->stack = a_stack_cpy;
+	a->last = ft_lstlast(a->stack);
 	ft_putendl_fd("done<------------\n", 1);
 	return (history);
 }
@@ -34,26 +47,37 @@ t_list *russian_algorithm(t_meta_stack *a, enum markup_mode markup_mode)
 
 int	main(int argc, const char **argv)
 {
-	t_meta_stack	*int_input;
-	t_list			*history;
+	t_meta_stack	*a;
+	t_list			*history_value_mode;
+	t_list			*history_index_mode;
 
 	if (argc < 2)
 	{
 		ft_putendl_fd("not enough arguments", 2);
 		return (0);
 	}
-	int_input = generate_stack(argc, argv + 1);
-	if (! int_input)
+	a = generate_stack(argc, argv + 1);
+
+	if (! a)
 	{
 		ft_putendl_fd("Error",2);
 		return (0);
 	}
-	history = russian_algorithm(int_input, value_mode);
-	ft_lstput_str_bonus(history);
-	ft_putendl_fd("operations needed:", 1);
-	ft_putnbr_fd(ft_lstsize(history), 1);
-	ft_lstclear(&history, do_not_free_content);
-	ft_lstclear(&int_input->stack, free);
-	free(int_input);
+	//history_index_mode = russian_algorithm(a, index_mode);
+	history_value_mode = russian_algorithm(a, value_mode);
+		ft_putendl_fd("------------value mode------------", 1);
+	ft_lstput_str_bonus(history_value_mode);
+		ft_putendl_fd("------------index mode------------", 1);
+	ft_lstput_str_bonus(history_index_mode);
+	ft_putstr_fd("operations needed for value mode: ", 1);
+	ft_putnbr_fd(ft_lstsize(history_value_mode), 1);
+	ft_putstr_fd("\n", 1);
+	ft_putstr_fd("operations needed for index mode: ", 1);
+	ft_putnbr_fd(ft_lstsize(history_index_mode), 1);
+	ft_putstr_fd("\n", 1);
+	ft_lstclear(&history_value_mode, do_not_free_content);
+	ft_lstclear(&history_index_mode, do_not_free_content);
+	ft_lstclear(&a->stack, free);
+	free(a);
 }
 #endif
--- src/sort_operation_fill_b.c
+++ src/sort_operation_fill_b.c
@@ -22,19 +22,24 @@ void fill_b(t_meta_stack *a, t_meta_stack *b, t_list **history,
 		return ;
 	all_prevs_are_properly_set(a);
 	markup_reference = calc_markup_reference(a, markup_mode);
-	markup_all_elements_according_to_reference(a, markup_reference);
+	markup_stack_by_reference(a, markup_reference, markup_mode);
 	all_prevs_are_properly_set(a);
-	while (a->stack && count_elements_to_be_moved_to_b(a->stack))
+	while (a->stack && count_elements_to_be_moved_to_b(a))
 	{
-		markup_all_elements_according_to_reference(a, markup_reference);
-		if (is_swapping_a_good_idea(a, markup_reference))
+		markup_stack_by_reference(a, markup_reference,
+								  markup_mode);
+		if (is_swapping_a_good_idea(a, markup_reference, value_mode))
 		{
 			swap_first_two_elements(a, history);
-			reverse_rotate(a, history);
+//			reverse_rotate(a, history);
+			rotate(a, history);
 		}
 		else if (! CONTENT_OF_ELEMENT(a->stack)->should_stay_on_stack_a)
 			push_first_element_to_the_other_stack(a, b, history);
 		else
-			reverse_rotate(a, history);
+		{
+//			reverse_rotate(a, history);
+			rotate(a, history);
+		}
 	}
 }
